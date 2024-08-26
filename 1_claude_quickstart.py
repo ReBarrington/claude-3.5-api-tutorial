4d8a537c4f16fe05a37787e05ffef0465a1f2a7f
Reagan Barrington <reagan.barrington@kingsmensoftware.com>
2024-08-26 14:37:52 -0400

    set up env

--- 1_claude_quickstart.py
+++ 1_claude_quickstart.py
@@ -1,6 +1,12 @@
 import anthropic
+import os
 
-client = anthropic.Anthropic()
+from dotenv import load_dotenv
+load_dotenv()
+
+api_key = os.getenv("ANTHROPIC_API_KEY")
+
+client = anthropic.Anthropic(api_key=api_key)
 
 message = client.messages.create(
     model="claude-3-5-sonnet-20240620",
